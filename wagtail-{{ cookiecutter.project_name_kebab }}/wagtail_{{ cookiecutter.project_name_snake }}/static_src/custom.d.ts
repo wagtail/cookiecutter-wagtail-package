148ec3befb16b9377abfa53d9572c98ca4474f52
Karl Hobley <karl@kaed.uk>
2020-11-02 17:12:16 +0000

    Fix gettext TypeScript definitions

--- wagtail-{{ cookiecutter.project_name_kebab }}/wagtail_{{ cookiecutter.project_name_snake }}/static_src/custom.d.ts
+++ wagtail-{{ cookiecutter.project_name_kebab }}/wagtail_{{ cookiecutter.project_name_snake }}/static_src/custom.d.ts
@@ -1,3 +1,5 @@
+export {};
+
 // Allows SVG files to be imported and used in TypeScript
 declare module '*.svg' {
     const content: any;
@@ -6,43 +8,66 @@ declare module '*.svg' {
 
 // Declare globals provided by Django's JavaScript Catalog
 // For more information, see: https://docs.djangoproject.com/en/3.1/topics/i18n/translation/#module-django.views.i18n
-declare module 'gettext' {
+declare global {
+    // Wagtail globals
+
+    interface WagtailConfig {
+        ADMIN_API: {
+            PAGES: string;
+            DOCUMENTS: string;
+            IMAGES: string;
+            EXTRA_CHILDREN_PARAMETERS: string;
+        };
+
+        I18N_ENABLED: boolean;
+        LOCALES: {
+            code: string;
+            /* eslint-disable-next-line camelcase */
+            display_name: string;
+        }[];
+    }
+
+    const wagtailConfig: WagtailConfig;
+
+    // Django i18n utilities
+
     // https://docs.djangoproject.com/en/3.1/topics/i18n/translation/#gettext
-    export default function gettext(text: string): string;
+    function gettext(text: string): string;
 
     // https://docs.djangoproject.com/en/3.1/topics/i18n/translation/#ngettext
-    export default function ngettext(singular: string, plural: string, count: number): string;
+    function ngettext(singular: string, plural: string, count: number): string;
 
     // https://docs.djangoproject.com/en/3.1/topics/i18n/translation/#interpolate
     // FIXME export default function interpolate(...): string;
 
     // https://docs.djangoproject.com/en/3.1/topics/i18n/translation/#get-format
     type FormatType =
-        'DATE_FORMAT' |
-        'DATE_INPUT_FORMATS' |
-        'DATETIME_FORMAT' |
-        'DATETIME_INPUT_FORMATS' |
-        'DECIMAL_SEPARATOR' |
-        'FIRST_DAY_OF_WEEK' |
-        'MONTH_DAY_FORMAT' |
-        'NUMBER_GROUPING' |
-        'SHORT_DATE_FORMAT' |
-        'SHORT_DATETIME_FORMAT' |
-        'THOUSAND_SEPARATOR' |
-        'TIME_FORMAT' |
-        'TIME_INPUT_FORMATS' |
-        'YEAR_MONTH_FORMAT';
-    export default function get_format(formatType: FormatType): string;
+        | 'DATE_FORMAT'
+        | 'DATE_INPUT_FORMATS'
+        | 'DATETIME_FORMAT'
+        | 'DATETIME_INPUT_FORMATS'
+        | 'DECIMAL_SEPARATOR'
+        | 'FIRST_DAY_OF_WEEK'
+        | 'MONTH_DAY_FORMAT'
+        | 'NUMBER_GROUPING'
+        | 'SHORT_DATE_FORMAT'
+        | 'SHORT_DATETIME_FORMAT'
+        | 'THOUSAND_SEPARATOR'
+        | 'TIME_FORMAT'
+        | 'TIME_INPUT_FORMATS'
+        | 'YEAR_MONTH_FORMAT';
+
+    function get_format(formatType: FormatType): string;
 
     // https://docs.djangoproject.com/en/3.1/topics/i18n/translation/#gettext_noop
-    export default function gettext_noop(text: string): string;
+    function gettext_noop(text: string): string;
 
     // https://docs.djangoproject.com/en/3.1/topics/i18n/translation/#pgettext
-    export default function pgettext(context: string, text: string): string;
+    function pgettext(context: string, text: string): string;
 
     // https://docs.djangoproject.com/en/3.1/topics/i18n/translation/#npgettext
-    export default function pgettext(context: string, text: string, count: number): string;
+    function pgettext(context: string, text: string, count: number): string;
 
     // https://docs.djangoproject.com/en/3.1/topics/i18n/translation/#pluralidx
-    export default function pluralidx(count: number): boolean;
+    function pluralidx(count: number): boolean;
 }
