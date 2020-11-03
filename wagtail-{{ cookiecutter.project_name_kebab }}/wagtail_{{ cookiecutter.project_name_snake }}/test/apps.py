2cc708a1340c039e827e307a95d82107ea01b47f
Karl Hobley <karl@kaed.uk>
2020-11-03 14:08:22 +0000

    Update apps.py

--- wagtail-{{ cookiecutter.project_name_kebab }}/wagtail_{{ cookiecutter.project_name_snake }}/test/apps.py
+++ wagtail-{{ cookiecutter.project_name_kebab }}/wagtail_{{ cookiecutter.project_name_snake }}/test/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class Wagtail{{ cookiecutter.project_name_camel }}TestAppConfig(AppConfig):
     label = "wagtail_{{ cookiecutter.project_name_snake }}_test"
     name = "wagtail_{{ cookiecutter.project_name_snake }}.test"
-    verbose_name = "{{ cookiecutter.project_name }} tests"
+    verbose_name = "Wagtail {{ cookiecutter.project_name }} tests"
