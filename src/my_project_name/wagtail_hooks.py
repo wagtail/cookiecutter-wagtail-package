ff4b61079281a8a5dcc05bfc9e946fab8c4199f1
Thibaud Colas <thibaudcolas@gmail.com>
2026-04-09 09:10:55 +0100

    Add simple hook to easily check the package is installed

--- src/my_project_name/wagtail_hooks.py
+++ src/my_project_name/wagtail_hooks.py
@@ -1,6 +1,17 @@
 from django.urls import include, path
 from django.views.i18n import JavaScriptCatalog
 from wagtail import hooks
+from wagtail.admin.site_summary import SummaryItem
+
+
+class MyProjectNameSummaryItem(SummaryItem):
+    order = 50
+    template_name = "my_project_name/admin/my_project_name_summary.html"
+
+
+@hooks.register("construct_homepage_summary_items")
+def register_my_project_name_summary_item(request, summary_items):
+    summary_items.append(MyProjectNameSummaryItem(request))
 
 
 @hooks.register("register_admin_urls")
